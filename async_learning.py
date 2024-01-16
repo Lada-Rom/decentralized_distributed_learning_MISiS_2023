30849d5b871bcf7c3a7f3e86458c533f1e2d662a
Lada-Rom <doodleztoon@google.com>
2024-01-16 15:11:17 +0300

    ENH: logging learning time info

--- async_learning.py
+++ async_learning.py
@@ -42,7 +42,7 @@ class ParamLoader:
         self.accum_need_ready_neighs_frac   = config_obj["accum_need_ready_neighs_frac"]
         self.accum_func_params              = config_obj["accum_func_params"]
         self.accum_initial_self_weight      = config_obj["accum_initial_self_weight"]
-        
+
         self.log_interval       = config_obj["log_interval"]
         self.log_topo           = config_obj["log_topo"]
         self.log_weights_accum  = config_obj["log_weights_accum"]
@@ -57,7 +57,7 @@ class Dataset:
     def __init__(self, params):
         data_folder_loc = os.path.join(
             os.path.dirname(os.path.abspath(__file__)), "..")
-        
+
         # train dataset
         self.train_dataset = datasets.MNIST(
             os.path.join(data_folder_loc, "data", "data-%d" % bf.rank()),
@@ -69,7 +69,7 @@ class Dataset:
 
         self.train_sampler = torch.utils.data.distributed.DistributedSampler(
             self.train_dataset, num_replicas=bf.size(), rank=bf.rank())
-        
+
         self.train_loader = torch.utils.data.DataLoader(
             self.train_dataset, batch_size=params.train_batch_size,
             sampler=self.train_sampler, **{})
@@ -85,18 +85,39 @@ class Dataset:
         self.test_sampler = None
         self.test_sampler = torch.utils.data.distributed.DistributedSampler(
             self.test_dataset, num_replicas=bf.size(), rank=bf.rank())
-        
+
         self.test_loader = torch.utils.data.DataLoader(
             self.test_dataset, batch_size=params.test_batch_size,
             sampler=self.test_sampler, **{})
 
 
 class Topo:
+    def log_topo_type(self, params):
+        if bf.rank() != 0:
+            return
+        print()
+        if params.topo_enable_dynamic is True:
+            print("Dynamic topology setted.")
+        elif params.topo_static_kind == 0:
+            print("Static topology: FullyConnectedGraph.")
+        elif params.topo_static_kind == 1:
+            print(f"Static topology: RingGraph (direction {params.topo_static_ring_direction}).")
+        elif params.topo_static_kind == 2:
+            print(f"Static topology: StarGraph (center on {params.topo_static_star_center} rank).")
+        elif params.topo_static_kind == 3:
+            if params.topo_static_meshgrid_size == (0, 0):
+                print(f"Static topology: MeshGrid2DGraph.")
+            else:
+                print(f"Static topology: MeshGrid2DGraph (size {params.topo_static_meshgrid_size}).")
+        elif params.topo_static_kind == 4:
+            print(f"Static topology: SymmetricExponentialGraph (base {params.topo_static_exp_base}).")
+        elif params.topo_static_kind == 5:
+            print(f"Static topology: ExponentialGraph (base {params.topo_static_exp_base}).")
+
     def __init__(self, params):
         self.dynamic_topo = None
 
         if params.topo_enable_dynamic is True:
-            print("Dynamic topology setted.")
             self.dynamic_topo = topology_util.\
                 GetDynamicSendRecvRanks(bf.load_topology(), bf.rank())
         else:
@@ -107,35 +128,22 @@ class Topo:
             # 4 = SymmetricExponentialGraph (base)
             # 5 = ExponentialGraph (base)
             if params.topo_static_kind == 0:
-                if bf.rank() == 0:
-                    print("Static topology: FullyConnectedGraph.")
                 bf.set_topology(topology_util.FullyConnectedGraph(bf.size()))
             elif params.topo_static_kind == 1:
-                if bf.rank() == 0:
-                    print(f"Static topology: RingGraph (direction {params.topo_static_ring_direction}).")
                 bf.set_topology(topology_util.RingGraph(bf.size()), params.topo_static_ring_direction)
             elif params.topo_static_kind == 2:
-                if bf.rank() == 0:
-                    print(f"Static topology: StarGraph (center on {params.topo_static_star_center} rank).")
                 bf.set_topology(topology_util.StarGraph(bf.size()), params.topo_static_star_center)
             elif params.topo_static_kind == 3:
                 if params.topo_static_meshgrid_size == (0, 0):
-                    if bf.rank() == 0:
-                        print(f"Static topology: MeshGrid2DGraph.")
                     bf.set_topology(topology_util.MeshGrid2DGraph(bf.size()))
                 else:
-                    if bf.rank() == 0:
-                        print(f"Static topology: MeshGrid2DGraph (size {params.topo_static_meshgrid_size}).")
                     bf.set_topology(topology_util.MeshGrid2DGraph(bf.size()), params.topo_static_meshgrid_size)
             elif params.topo_static_kind == 4:
-                if bf.rank() == 0:
-                    print(f"Static topology: SymmetricExponentialGraph (base {params.topo_static_exp_base}).")
                 bf.set_topology(topology_util.SymmetricExponentialGraph(bf.size()), params.topo_static_exp_base)
             elif params.topo_static_kind == 5:
-                if bf.rank() == 0:
-                    print(f"Static topology: ExponentialGraph (base {params.topo_static_exp_base}).")
                 bf.set_topology(topology_util.ExponentialGraph(bf.size()), params.topo_static_exp_base)
 
+        self.log_topo_type(params)
         if params.log_topo == True:
             print(f"topo [{bf.rank()}]: in {bf.in_neighbor_ranks()}, out {bf.out_neighbor_ranks()}")
 
@@ -174,7 +182,7 @@ class Net(nn.Module):
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
         return F.log_softmax(x, dim=0)
-    
+
     def win_create_weights(self):
         for name, tensor in sorted(self.state_dict().items()):
             bf.win_create(tensor, name=name, zero_init=True)
@@ -184,15 +192,15 @@ class Net(nn.Module):
     def win_put_weights(self):
         self.event_number += 1
         for name, tensor in sorted(self.state_dict().items()):
-            bf.win_put(tensor, name=name)
+           bf.win_put(tensor, name=name, require_mutex=True)
         timestamp = torch.FloatTensor([time.time(), self.event_number])
-        bf.win_put(timestamp, name=self.name_time)
+        bf.win_put(timestamp, name=self.name_time, require_mutex=True)
 
     def win_update_weights(self, in_neighbors):
         dst_weights = {}
         for rank in in_neighbors:
             last_weight_time = bf.win_update(name=self.name_time,
-                    self_weight=0., neighbor_weights={rank: 1})
+                    self_weight=0., neighbor_weights={rank: 1}, require_mutex=True)
             event_delta = self.event_number - last_weight_time[1]
             weight_time_delta = time.time() - last_weight_time[0] \
                               + (0 if event_delta < 0 else 2 * (event_delta))
@@ -203,7 +211,7 @@ class Net(nn.Module):
         for rank, weight in dst_weights.items():
             dst_weights.update({rank: (1 - self_weight) * weight / (norm)})
         self_weight = 1 - sum(dst_weights.values())
-        
+
         if params.log_weights_accum == True:
             round_dst_weights = {r: round(x, 4) for (r, x) in dst_weights.items()}
             print(f"weights: [{bf.rank()}]: self {round(self_weight, 4)}, "
@@ -212,7 +220,7 @@ class Net(nn.Module):
 
         for name, tensor in sorted(self.state_dict().items()):
             bf.win_update(name=name,
-                self_weight=self_weight, neighbor_weights=dst_weights)
+                self_weight=self_weight, neighbor_weights=dst_weights, require_mutex=True)
             self.state_dict()[name].data[:] = tensor
 
 
@@ -226,7 +234,7 @@ class NodeNetwork:
         self.optimizer = optim.SGD(self.model.parameters(), lr=params.train_lr * bf.size())
         bf.broadcast_parameters(self.model.state_dict(), root_rank=0)
         bf.broadcast_optimizer_state(self.optimizer, root_rank=0)
-        
+
         self.topo = Topo(params)
         self.model.win_create_weights()
 
@@ -237,17 +245,19 @@ class NodeNetwork:
 
     def _prepare_to_train(self):
         bf.barrier()
+        if bf.rank() == 0:
+            print()
         self.model.event_number = 0
 
         self.stop_train = torch.FloatTensor([0.])
         bf.win_create(self.stop_train, name=self.name_stop_train, zero_init=True)
-        bf.win_put(self.stop_train, name=self.name_stop_train)
+        bf.win_put(self.stop_train, name=self.name_stop_train, require_mutex=True)
         if params.log_early_stop == True:
             print(f"stop [{bf.rank()}]: before train {self.stop_train}")
 
     def _prepare_to_test(self):
         self.stop_train = torch.FloatTensor([1.])
-        bf.win_put(self.stop_train, name=self.name_stop_train)
+        bf.win_put(self.stop_train, name=self.name_stop_train, require_mutex=True)
         if params.log_early_stop == True:
             print(f"stop [{bf.rank()}]: before test {self.stop_train}")
 
@@ -259,7 +269,7 @@ class NodeNetwork:
             ready_nodes = []
             for rank in from_neighbors:
                 last_weight_time = bf.win_update(name=name_time,
-                    self_weight=0., neighbor_weights={rank: 1})
+                    self_weight=0., neighbor_weights={rank: 1}, require_mutex=True)
                 if self.model.event_number <= last_weight_time[1]:
                     ready_nodes.append(rank)
             if params.log_ready_nodes == True:
@@ -273,7 +283,7 @@ class NodeNetwork:
         self.model.train()
         dataset.train_sampler.set_epoch(epoch)
         for batch_idx, (data, target) in enumerate(dataset.train_loader):
-            self.stop_train = bf.win_update(name=self.name_stop_train, reset=True)
+            self.stop_train = bf.win_update(name=self.name_stop_train, reset=True, require_mutex=True)
 
             if self.stop_train > 0.0001:
                 if params.log_early_stop == True:
@@ -301,7 +311,7 @@ class NodeNetwork:
                         100.0 * batch_idx / len(dataset.train_loader),
                         loss.item()))
 
-    def test(self, dataset, record):
+    def test(self, dataset, test_record, time_record):
         self._prepare_to_test()
 
         self.model.eval()
@@ -323,10 +333,14 @@ class NodeNetwork:
         test_loss = self._metric_average(test_loss, "avg_loss")
         test_accuracy = self._metric_average(test_accuracy, "avg_accuracy")
 
+        test_record.append((round(test_loss, 4), round(100.0 * test_accuracy, 4)))
+        time_record[-1] = round(time.time() - time_record[-1], 4)
         if bf.rank() == 0:
-            print("\nTest set: Average loss: {:.4f}, Accuracy: {:.2f}%\n".format(
+            print("\nTest set: Average loss: {:.4f}, Accuracy: {:.2f}%".format(
                 test_loss, 100.0 * test_accuracy), flush=True)
-        record.append((round(test_loss, 4), round(100.0 * test_accuracy, 4)))
+        else:
+            time.sleep(0.001)
+        print(f"Epoch time on rank [{bf.rank()}]: {time_record[-1]} sec")
         bf.win_free(name=self.name_stop_train)
 
     def finish(self):
@@ -343,14 +357,21 @@ if __name__=="__main__":
     distr_network = NodeNetwork(params)
 
     test_record = []
+    time_record = []
     for epoch in range(1, params.train_epochs + 1):
+        time_record.append(time.time())
         distr_network.train(data)
-        distr_network.test(data, test_record)
+        distr_network.test(data, test_record, time_record)
 
     distr_network.finish()
     bf.barrier()
 
-    print("Rank: (Test loss, Test accuracy(%)), ...") if bf.rank() == 0 else time.sleep(0.01)
-    print(f"[{bf.rank()}]: ", test_record)
+    distr_network.topo.log_topo_type(params)
+    print("\n[Rank]: [(Test loss, Test accuracy(%)), ...]") if bf.rank() == 0 else time.sleep(0.01)
+    print(f"[{bf.rank()}]:   ", test_record)
+    bf.barrier()
+
+    print("\n[Rank]: (Total) [Time on epoch (sec), ...]") if bf.rank() == 0 else time.sleep(0.01)
+    print(f"[{bf.rank()}]:   ({round(sum(time_record), 4)})", time_record)
 
     bf.win_free()
